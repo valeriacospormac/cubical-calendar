95b0d3f77af17a07b44b681b0198e05759cf17ba
valeriacospormac <valeriacospormac@gmail.com>
2017-11-03 09:10:29 +0200

    change style

--- src/app/components/calendar-handler/calendar-form/calendar-form.module.ts
+++ src/app/components/calendar-handler/calendar-form/calendar-form.module.ts
@@ -1,6 +1,7 @@
 import { module } from 'angular';
 import {thisModuleName} from '../../../config';
 import {CalendarFormComponent} from './calendar-form.component';
+import './_calendar-form.scss';
 
 export const CalendarFormModule = module(thisModuleName + __dirname, [])
   .component('ccCalendarForm', CalendarFormComponent)
